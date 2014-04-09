449c42b9668bc57ae48e2a2befe55ef24324ad7d
Bjorn Pardo <bjorn@xend.com.ph>
2014-04-09 15:43:04 -0600

    apr9343

--- main.js
+++ main.js
@@ -12,10 +12,19 @@ var turnBlack=function(){
 };
 
 // Attaching an Event Handler
-$('h1').on('mouseover',function(e){
-	$(e.target).css('color','green');
+// $('h1').on('mouseover',function(e){
+// 	$(e.target).css('color','green');
+// });
+
+$('h1').on('mouseover',function(){
+	$(this).css('color','green');
 });
-//Anonymous function- Above example when you replace function with the function expression
+
+// e.target = this
+// e.preventDefault = return false;
+// No need for e in function()
+// e.xxx is native and not crossbrowser compatible while this and return false is jquery and crossbrowser compatiable.
+//Anonymous function- Above example when you replace function with the function expression which is the best practice
 
 $('h1').on('mouseout',turnBlack);
 
